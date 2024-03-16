40462d6623c28d45fc1bb64b9734bf3267ded531
Kuznetsov-Dmitrii <dimanshik66@mail.ru>
2024-03-16 22:09:50 +0400

    создан eureka-client. eureka-server видит eureka-client.

---
+++ Salon/eureka-client/src/main/java/com/example/eurekaclient/Controller.java
@@ -0,0 +1,12 @@
+package com.example.eurekaclient;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/test")
+    public String tes(){
+        return "t";
+    }
+}
